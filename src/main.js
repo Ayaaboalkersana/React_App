8d0adbcefaabc34d6b4203c3aa61b4fbe82ab1da
Ayaaboalkersana <ayaaboalkrsana@gmail.com>
2023-10-02 15:37:07 +0300

    Lab 5

--- src/main.js
+++ src/main.js
@@ -1,19 +1,29 @@
 import CardComp from './card';
 import data from './data.json';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Form } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import './main.css';
 
 
 function Main() {
-    let [items,setItems]= useState(data);
+    let [items,setItems]= useState([]);
 
+    async function getData(){
+      const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=b');
+      const result = await response.json();
+      setItems(result.meals)
+
+
+    }
+
+    useEffect(function (){getData()},[])
+  
     function handelSubmit(event){
         event.preventDefault()
         let searchValue = event.target.search.value;
         let filteredItems = data.filter(function(item){return item.title.toLowerCase().includes(searchValue.toLowerCase())})
-        setItems(filteredItems)
+        setItems(filteredItems);
 
     }
   return (
@@ -31,7 +41,7 @@ function Main() {
     <div id= "cardContainer" >
     {items.map(function(item){
         return(
-            <CardComp image_url={item.image_url} title={item.title} description={item.description}/>
+            <CardComp image_url={item.strMealThumb} title={item.strMeal} />
 
 )})}
 </div>
